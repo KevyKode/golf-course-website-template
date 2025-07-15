6c32e02ef4d917b13ba62b07ae583b01bdd4e02d
KevyKode <134665346+KevyKode@users.noreply.github.com>
2025-07-15 12:01:47 -0600

    server update js

--- server.js
+++ server.js
@@ -1,38 +1,22 @@
 const express = require('express');
 const path = require('path');
-const compression = require('compression');
-const helmet = require('helmet');
 const app = express();
 
-// Security headers
+// Security and compression middleware
+const helmet = require('helmet');
+const compression = require('compression');
+
 app.use(helmet({
-  contentSecurityPolicy: false, // Disable CSP if it causes issues with your resources
+  contentSecurityPolicy: false
 }));
-
-// Compress responses
 app.use(compression());
-
-// Parse JSON bodies
-app.use(express.json());
-
-// Parse URL-encoded bodies
-app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the public directory
 app.use(express.static('public'));
 
-// Custom routes can go here
-// app.get('/api/example', (req, res) => { ... });
-
-// For all other routes, send the index.html file (for single-page applications)
+// For all other routes, send the index.html file
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-// Error handling
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
 });
 
 // Start the server
